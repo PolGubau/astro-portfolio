1acf27478ab791e4af369dd6440ed3259b2aebe0
Pol Gubau Amores <gubaupol@gmail.com>
2025-04-13 10:07:53 +0200

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://polgubau.com',
+    prefetch: true,
   integrations: [mdx(), sitemap()],
 
   vite: {
--- src/content.config.ts
+++ src/content.config.ts
@@ -15,23 +15,24 @@ const blog = defineCollection({
 
 const projects = defineCollection({
 	loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
-	schema: z.object({
-		title: z.string(),
-		summary: z.string(),
-		startedAt: z.string().transform((str) => new Date(str)),
-		endedAt: z
-			.string()
-			.transform((str) => new Date(str))
-			.optional(),
-		color: z.string(),
-		link: z.string().optional(),
-		githubLink: z.string().optional(),
-		npmCommand: z.string().optional(),
-		npmLink: z.string().optional(),
-		cover: z.string(),
-		category: z.string(),
-		tech: z.array(z.string()).optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			summary: z.string(),
+			startedAt: z.string().transform((str) => new Date(str)),
+			endedAt: z
+				.string()
+				.transform((str) => new Date(str))
+				.optional(),
+			color: z.string(),
+			link: z.string().optional(),
+			githubLink: z.string().optional(),
+			npmCommand: z.string().optional(),
+			npmLink: z.string().optional(),
+			cover: image(),
+			category: z.string(),
+			tech: z.array(z.string()).optional(),
+		}),
 });
 
 export type Project = InferEntrySchema<"projects">;
